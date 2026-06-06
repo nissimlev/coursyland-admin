45f2602e3fac6f18fefbaaf5089fa6acb73ceaf9
Nissim Levi <nissimlevi@h-MacBook-Air-sl-Nissim-4.local>
2026-06-06 10:29:03 +0300

    Debug: test multiple iCount endpoints

--- api/debug_icount.php
+++ api/debug_icount.php
@@ -24,24 +24,44 @@ foreach ($courses as $course) {
         'doc_type'        => 320,
     ];
 
+    // נסה כמה endpoints שונים
+    $endpoints = [
+        'https://api.icount.co.il/api/v3.php/doc/getList',
+        'https://api.icount.co.il/api/v3.php/payment_page/getDocList',
+        'https://api.icount.co.il/api/v3.php/client/getList',
+    ];
+
     $ch = curl_init();
     curl_setopt_array($ch, [
-        CURLOPT_URL            => 'https://api.icount.co.il/api/v3.php/doc/getList',
+        CURLOPT_URL            => $endpoints[0],
         CURLOPT_POST           => true,
         CURLOPT_POSTFIELDS     => http_build_query($params),
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT        => 30,
+        CURLOPT_SSL_VERIFYPEER => false,
     ]);
     $response = curl_exec($ch);
     $err      = curl_error($ch);
     curl_close($ch);
+
+    // נסה גם GET
+    $getUrl = 'https://api.icount.co.il/api/v3.php/doc/getList?' . http_build_query($params);
+    $ch2 = curl_init($getUrl);
+    curl_setopt_array($ch2, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_SSL_VERIFYPEER => false,
+    ]);
+    $responseGet = curl_exec($ch2);
+    curl_close($ch2);
 
     $results[] = [
         'course'           => $course['name'],
         'payment_page_id'  => $course['icount_payment_page_id'],
         'params_sent'      => $params,
         'curl_error'       => $err,
-        'raw_response'     => json_decode($response, true),
+        'post_response'    => json_decode($response, true),
+        'get_response'     => json_decode($responseGet, true),
     ];
 }
 
